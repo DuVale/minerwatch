e7cd3ad8654c09b2dd880fd1689bb3afad7b47bd
themattreid <themattreid@15318720-9f6d-a5d3-0f5b-58857496ec34>
2011-07-06 00:16:52 +0000

    update

--- trunk/system/application/controllers/main.php
+++ trunk/system/application/controllers/main.php
@@ -18,7 +18,8 @@ class Main extends Controller {
   function monitor_slush() {      
     log_message('debug', "main_controller_monitor_slush function called");
     $this->load->model('Model_main', 'main');
-    $g['data'] = $this->main->get_data();
+    $g['data'] = $this->main->slush_get_current();
+    $g['daily'] = $this->main->slush_get_daily();
     $g['root'] = $this->config->item('base_url');
     $g['home'] = $this->config->item('base_dir');
     #$_SESSION['example'] = $g['root'];    // we set this for the header files that don't use CI sessions
--- trunk/system/application/models/model_main.php
+++ trunk/system/application/models/model_main.php
@@ -15,32 +15,46 @@ class Model_main extends Model
     parent::Model();
   }
   
-  function enterstate() {    
-    $MYSQL_VERSION = $this->db->escape_str($MYSQL_VERSION);
+  function slush_get_current() {
+    log_message('debug', "Starting slush_get_current");
     $dbr = $this->load->database('main', TRUE);
-    $sql0="INSERT INTO `dbbenchmark`.`data` (
-`id` ,
-`creation_time`
-)
-VALUES (
-NULL,
-NOW())";
-    $this->db->trans_start();
-    log_message('debug', "Transaction: $sql0");
-    $this->db->query($sql0);
-    $this->db->trans_complete();
-    if ($this->db->trans_status() === FALSE) {
-      log_message('debug', "Transaction failed: $sql0");
-      $this->db->trans_off();
-      return 1;
-    } 
-    else {
-      $this->db->trans_off();
-      return 0;
+    $sql = "select * from api_keys";
+    log_message('debug', "$sql");
+    $data = array();
+    $query = $dbr->query($sql);    
+    if($query->num_rows() > 0) {
+      foreach ($query->result() as $row) {
+        $a = $row->api_key;;
+        $sql1 = "select * from slush_pool_results where active='0' and api_key='$a' order by worker_name";
+        log_message('debug', "$sql1");
+        $query1 = $dbr->query($sql1);
+        if($query1->num_rows() > 0) {
+	  $unconfirmed_reward = $row1->unconfirmed_reward;
+	  $confirmed_reward = $row1->confirmed_reward;
+	  $worker_name = $row1->worker_name;
+	  $last_share = $row1->last_share;
+	  $score = $row1->score;
+	  $alive = $row1->alive;
+	  $shares = $row1->shares;
+	  $hashrate = $row1->hashrate;
+	  $creation_time = $row1->creation_time;
+	  $z = array("worker_name" => $worker_name,
+		     "unconfirmed_reward" => $unconfirmed_reward,
+		     "confirmed_reward" => $confirmed_reward,
+		     "last_share" => $last_share,
+		     "score" => $score,
+		     "alive" => $alive,
+		     "shares" => $shares,
+		     "hashrate" => $hashrate,
+		     "creation_time" => $creation_time);
+	  array_push($data,$z);	  
+	}
+      }
     }
+    return $data;
   }
 
-  function get_data() {
+  function slush_get_daily() {
     log_message('debug', "Starting get_data");
     $dbr = $this->load->database('main', TRUE);
     $sql = "select * from api_keys";
@@ -58,7 +72,7 @@ NOW())";
 	    $api_key = $row1->api_key;
 	    $worker_name = $row1->worker_name;
 
-	    $sql2 = "select * from slush_pool_results where active='0' and api_key='$a' order by worker_name";
+	    $sql2 = "select * from slush_pool_results_archive where worker_name='$worker_name' and api_key='$a' AND creation_time BETWEEN DATE_SUB(NOW(), INTERVAL 1 DAY) AND NOW()";
 	    log_message('debug', "$sql2");
 	    $query2 = $dbr->query($sql2);
 	    if($query2->num_rows() > 0) {
@@ -73,9 +87,6 @@ NOW())";
 		$hashrate = $row2->hashrate;
 		$creation_time = $row2->creation_time;
 		$z = array("worker_name" => $worker_name,
-			   "unconfirmed_reward" => $unconfirmed_reward,
-			   "confirmed_reward" => $confirmed_reward,
-			   "last_share" => $last_share,
 			   "score" => $score,
 			   "alive" => $alive,
 			   "shares" => $shares,
